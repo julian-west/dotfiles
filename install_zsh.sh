542e82c36d3fb5fbf756a36508e3bb673f0e8988
Julian West <julian.west@datatonic.com>
2020-11-26 14:16:01 +0000

    update install script

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,13 +2,16 @@
 # script to install zsh
 
 # install zsh
+set -e
+
+DIR=$PWD
+
 echo "Installing zsh..."
-sudo apt install zsh
+sudo apt -y install zsh
 
 # install oh-my-zsh
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 
 # install zsh-syntax-highlighting
@@ -23,7 +26,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 
 echo "Installations complete"
 
-zsh
-chsh -s $(which zsh)
-. $HOME/dotfiles/.zshrc
-
+cd $DIR
