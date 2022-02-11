08dcc323249446b9713669d4407266fa2811abc3
julian-west <westjc@btopenworld.com>
2022-02-11 10:12:30 +0000

    fix depreciation error for lspkind init

--- nvim/.config/nvim/after/plugin/lspkind.rc.lua
+++ nvim/.config/nvim/after/plugin/lspkind.rc.lua
@@ -5,7 +5,8 @@ lspkind.init({
   -- enables text annotations
   --
   -- default: true
-  with_text = true,
+  -- with_text = true,
+  mode = "symbol_text",
 
   -- default symbol map
   -- can be either 'default' (requires nerd-fonts font) or
