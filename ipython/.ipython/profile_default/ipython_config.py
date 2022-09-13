c7ec16b7a068e8cb0281d4c4fca82c55c779d2eb
julian-west <westjc@btopenworld.com>
2022-09-13 19:43:05 +0100

    Add ipython configuration files

---
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+
+print("--------->>>>>>>> AUTORELOAD ENABLED <<<<<<<<<------------")
